dd1c35b1cb074bb0b39f90e913e82817e3dfbb16
Jeremy Smith <j.smith.03@cantab.net>
2017-06-30 09:25:00 -0700

    Added info text and scale circles

--- visualization/US_map_airports.js
+++ visualization/US_map_airports.js
@@ -1,29 +1,43 @@
 // US map airports for Delay Mapper
 // Updated for d3 v4
 
+// Function for scaling circle radii
+function scalingFunction(n) {
+    return 0.25 * Math.pow(n, 1/4);
+}
+
 // Setup variables
 var w = 1280;
 var h = 800;
+
+var circleRadii = [100000, 5000000, 50000000];
 
 var menu = [{
     "title": "Season",
     "items": ["Spring", "Summer", "Fall", "Winter"]
     }, {
     "title": "Airline",
-    "items": ["All", "American", "Delta", "Southwest", "United", "Alaska", "JetBlue", "Spirit"],
+    "items": ["All", "American", "Delta", "Southwest",
+            "United", "Alaska", "JetBlue", "Spirit"],
     "codes": ["ALL", "AA", "DL", "WN", "UA", "AS", "B6", "NK"]
     }, {
     "title": "Display",
-    "items": ["Arrival delay", "Taxi in time"]
+    "items": ["Percent delayed", "Arrival delay", "Taxi in time"],
+    "descr": ["% Flights delayed on arrival by more than 15 min",
+        "Average delay time on arrival (min)",
+        "Average taxi in time on arrival (min)"]
     }];
 
 var bodySelection = d3.select("body");
 
 var colorScalePercent = d3.scaleSequential(d3.interpolateRdYlGn)
-    .domain([24.0, 8.0]);
+    .domain([24.0, 10.0]);
+
+var colorScaleDelTime = d3.scaleSequential(d3.interpolateRdYlGn)
+    .domain([1.0, -10.0]);
 
 var colorScaleTime = d3.scaleSequential(d3.interpolateRdYlGn)
-    .domain([1.0, -10.0]);
+    .domain([10.0, 0.0]);
 
 // Projection and map path
 var path = d3.geoPath();
@@ -31,13 +45,17 @@ var projection = d3.geoAlbersUsa()
     .scale(1280)
     .translate([480, 300]);
 
-// Containers
+// Containers and titles
 var svgContainer = bodySelection.insert("svg")
     .attr("width", w)
     .attr("height", h);
 
 var titleMain = bodySelection.append("h2")
     .html("<span>U.S. Commercial Airports</span><br>Flight Delay Mapper");
+
+var titleInfo = bodySelection.append("h3")
+    .text("Click on airports to display color")
+    .attr("class", "info");
 
 var optionBar = bodySelection.append("div")
     .attr("class", "bar");
@@ -46,10 +64,18 @@ var titleAuthor = bodySelection.append("h3")
     .text("Jeremy Smith 2017");
 
 var scaleBarContainer = bodySelection.append("div")
-    .attr("class", "sb")
-    .append("svg")
+    .attr("class", "sbbot");
+
+var scaleBarsvgContainer = scaleBarContainer.append("svg")
     .attr("width", 400)
-    .attr("height", 70);
+    .attr("height", 75);
+
+var scaleCirclesContainer = bodySelection.append("div")
+    .attr("class", "sbright");
+
+var scaleCirclessvgContainer = scaleCirclesContainer.append("svg")
+    .attr("width", 200)
+    .attr("height", 50);
 
 // Option bar setup
 var optionBarTitle = optionBar.append("h3")
@@ -59,24 +85,39 @@ var optionBarItems = optionBar.selectAll("p")
     .data(menu)
     .enter()
     .append("p")
-    .text(function(d) {
+    .text(function(d, i) {
         return d.title;
     });
 
 // Scale bar setup
 var colorBar = d3.legendColor()
-    .labelFormat(d3.format(".0f"))
-    .title("% Delayed on arrival by more than 15 min")
+    .title(menu[2].descr[0])
+    .labels([">24","","22","","20","","18","","16","","14","","12","", "<10"])
     .ascending(true)
     .orient("horizontal")
-    .cells(9)
-    .shape("circle")
-    .shapePadding(15)
+    .cells(15)
+    .shape("rect")
+    .shapePadding(0)
     .scale(colorScalePercent);
 
-var scaleBar = scaleBarContainer.append("g")
+var scaleBar = scaleBarsvgContainer.append("g")
     .attr("transform", "translate(0,24)")
     .call(colorBar);
+
+var scaleCircles = scaleCirclessvgContainer.selectAll("circle")
+    .data(circleRadii)
+    .enter()
+    .append("svg:circle")
+    .attr("cx", 175)
+    .attr("cy", function(d) {
+        return 50 - scalingFunction(d);
+    })
+    .attr("r", function(d) {
+        return scalingFunction(d);
+    });
+
+var scaleTitle = scaleCirclesContainer.append("p")
+    .html("Passengers per year<br>1, 50, 500 hundred thousand");
 
 // States and airport circles
 var states = svgContainer.append("g")
@@ -95,7 +136,7 @@ d3.json("https://unpkg.com/us-atlas@1/us/10m.json", function(err, us) {
         .attr("d", path);
 });
 
-// Load aiport data from csv file
+// Load aiport and delay data from csv file
 d3.csv("airports_wdelaydata_ALL.csv", function(err, airports) {
     if (err) throw err;
 
@@ -117,7 +158,7 @@ d3.csv("airports_wdelaydata_ALL.csv", function(err, airports) {
             return d.locations[1];
         })
         .attr("r", function(d) {
-            return 0.2 * Math.pow(d.CY15enplane, 1/4);
+            return scalingFunction(d.CY15enplane);
         })
         .on("mouseover", function(d) {
             d3.select("h2 span").text(d.Name);
@@ -132,7 +173,6 @@ d3.csv("airports_wdelaydata_ALL.csv", function(err, airports) {
             } else {
                 d.highlighted = true;
                 color = colorScalePercent(d.PercentArrDel15);
-                //console.log(d.PercentArrDel15);
                 d3.select(this).style("fill", color);
             }
         });
